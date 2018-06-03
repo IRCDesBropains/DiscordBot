4ed958f2add5851af3bb457d8b04d3288cbe3c08
milow <mickael.bettinelli@etu.univ-lyon1.fr>
2018-06-03 16:59:49 +0200

    modif mineure

---
+++ Events/Blindbid.js
@@ -0,0 +1,17 @@
+/************************************************* 
+*              MODULE BLIND_BID                  *
+*************************************************/
+
+module.exports = function() {
+    var BlindBid = {};
+
+    /**
+     * @description 
+     * @param debug - Booléen
+     */
+    BlindBid.create = function(message, debug){
+        
+    };
+                                
+    return BlindBid;
+}();

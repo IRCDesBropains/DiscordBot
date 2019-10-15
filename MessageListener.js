c93485cb30d430acda002c8885ea55c38bde6cbc
milow <mickael.bettinelli@etu.univ-lyon1.fr>
2019-10-15 20:06:42 +0200

    ajout de feature HEURE

--- MessageListener.js
+++ MessageListener.js
@@ -19,6 +19,8 @@ module.exports = function() {
         client.on('message', message => {
             if (message.content === 'ping') {
                 message.reply('pong');
+                var d = new Date();
+                message.reply("Timestamp : " + d);
             }
             else if (message.content.startsWith("/say")) {
                 var msg = message.content.split("/say");
